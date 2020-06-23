207458eda26942fecec760287bda4e036e001a97
= <=>
2020-06-23 02:05:51 +0100

    Update registration form, duplicate username checker

--- application.py
+++ application.py
@@ -20,10 +20,7 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
         
-        # Check username exists
-        user_object = User.query.filter_by(username=username).first()
-        if user_object:
-            return "Someone else has taken this username!"
+        # Add user to DB
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
